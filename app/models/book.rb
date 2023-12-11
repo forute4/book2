4809761eb212fdcf343c218b2a56031e86436c21
EC2 Default User <ec2-user@ip-172-31-20-28.ap-northeast-1.compute.internal>
2023-12-11 04:49:13 +0000

    [middle3]4-25

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,11 @@
 class Book < ApplicationRecord
-  
+
+
   has_one_attached :book
-  
+
   belongs_to :user
-  
+  validates :title, presence: true
+  validates :body, presence: true
    def get_book
     unless book.attached?
       file_path = Rails.root.join('app/assets/books/no_image.jpg')
@@ -11,5 +13,5 @@ class Book < ApplicationRecord
     end
     book
    end
-  
+
 end
