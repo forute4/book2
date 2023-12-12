0f090de9a9c912e89fd7d2051dd474a4286c75b2
EC2 Default User <ec2-user@ip-172-31-20-28.ap-northeast-1.compute.internal>
2023-12-12 08:53:44 +0000

    [middle6]4-25

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   get 'homes/top'
   root to: "homes#top"
   get 'homes/about'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy, ]
   resources :users, only: [:show, :edit, :index, :update]
   
