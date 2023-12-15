36fd271eff823877f2fa7131471c2e8fa2279296
EC2 Default User <ec2-user@ip-172-31-20-28.ap-northeast-1.compute.internal>
2023-12-15 08:15:26 +0000

    [middle8]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,20 @@ class UsersController < ApplicationController
     @books =Book.all
   end
   
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  if @user.save
+    flash[:notice] ="User was successfully updated"
+    redirect_to user_path(@user.id)
+  else
+    flash[:alert] ="User was error updated "
+    render :edit
+  end
+  end
+  
   def index
     @users= User.all
   end
-  
-  
+
 end
